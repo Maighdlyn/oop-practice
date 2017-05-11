f11fb1794702cfaf403b517de67451ce2b64df91
Maighdlyn <emmamaighdlyn@gmail.com>
2017-05-10 18:01:15 -0700

    completed stage 3

--- bike-shop/src/stage3-methods.js
+++ bike-shop/src/stage3-methods.js
@@ -1,17 +1,52 @@
-function Bike() {
-  // your code here
+function Bike(rings = [3, 7]) {
+	this.tires = [new Tire(), new Tire()];
+	this.frame = new Frame();
+	this.moving = false;
+	this.rings = rings;
+}
+
+Bike.prototype.isMoving = function () {
+	return this.moving;
+}
+
+Bike.prototype.pedal = function () {
+	if ((this.tires[0].flat === true) || (this.tires[1].flat === true)) {
+		throw "Can't pedal with a flat tire"
+	}
+	this.moving = true;
+}
+
+Bike.prototype.brake = function () {
+	this.moving = false;
+}
+
+Bike.prototype.gearSpeeds = function () {
+	return this.rings[0] * this.rings[1];
 }
 
 function Frame() {
-  // your code here
 }
 
 function Tire() {
-  // your code here
+	this.flat = false;
+
+}
+
+Tire.prototype.isFlat = function () {
+	return this.flat;
+}
+
+Tire.prototype.puncture = function () {
+	this.flat = true;
+
+}
+
+Tire.prototype.repair = function () {
+	this.flat = false;
 }
 
 module.exports = {
-  Bike: Bike,
-  Frame: Frame,
-  Tire: Tire
+	Bike: Bike,
+	Frame: Frame,
+	Tire: Tire
 }
