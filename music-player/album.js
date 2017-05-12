fd75493f3f71115c3209ec786d86b772c93deb05
Maighdlyn <emmamaighdlyn@gmail.com>
2017-05-12 14:24:08 -0700

    completed stage5 music player

--- music-player/album.js
+++ music-player/album.js
@@ -1,1 +1,11 @@
-// Your code here
+class Album extends Artist {
+	constructor() {
+		this.title = title;
+		this.songCount = songCount;
+		this.songs = songs;
+		this.artist = artist;
+	}
+	songCount() {
+		return this.songs.length
+	}
+}
--- music-player/playlist.js
+++ music-player/playlist.js
@@ -1,1 +1,36 @@
-// Your code here
+class Playlist {
+	constructor({
+		playListName,
+		songs = []
+	}) {
+		this.playListName = playListName;
+		this.songs = songs;
+		this.currentSongIndex = 0;
+		this.currentSong = songs[currentSongIndex]
+	}
+
+	addSong() {
+		this.songs.push(song)
+	}
+	removeSong() {
+		const index = this.songs.indexOf(song)
+		if (index > -1) {
+			this.songs.splice(index, 1)
+		}
+	}
+	play() {
+		this.currentSong.play()
+	}
+
+	pause() {
+		this.currentSong.pause()
+	}
+
+	next() {
+		if (this.currentSongIndex < this.songs.length) {
+			this.currentSongIndex++;
+		} else {
+			this.currentSongIndex = 0
+		}
+	}
+}
